e80c4f3c0639ddb1f799f64299fecb4cb8e97a58
256hax <256hax@git>
2020-01-02 13:35:40 +0900

    chore: formatting

--- includes/admin/views/html-admin-options-page.php
+++ includes/admin/views/html-admin-options-page.php
@@ -12,30 +12,30 @@
 
 	<table class="form-table">
 	  <tr>
-		<th scope="row"><?php esc_html_e( 'Login Title', 'wp-similar-basic-auth' ); ?></th>
-		<td><input type="text" name="<?php echo $hax_wsba_config->register_settings_title; ?>" size="80" value="<?php echo esc_attr( get_option( $hax_wsba_config->register_settings_title ) ); ?>" /></td>
+  		<th scope="row"><?php esc_html_e( 'Login Title', 'wp-similar-basic-auth' ); ?></th>
+  		<td><input type="text" name="<?php echo $hax_wsba_config->register_settings_title; ?>" size="80" value="<?php echo esc_attr( get_option( $hax_wsba_config->register_settings_title ) ); ?>" /></td>
 	  </tr>
 	  <tr>
-		<th scope="row"><?php esc_html_e( 'Login Message', 'wp-similar-basic-auth' ); ?></th>
-		<td><input type="text" name="<?php echo $hax_wsba_config->register_settings_message; ?>" size="80" value="<?php echo esc_attr( get_option( $hax_wsba_config->register_settings_message ) ); ?>" /></td>
+  		<th scope="row"><?php esc_html_e( 'Login Message', 'wp-similar-basic-auth' ); ?></th>
+  		<td><input type="text" name="<?php echo $hax_wsba_config->register_settings_message; ?>" size="80" value="<?php echo esc_attr( get_option( $hax_wsba_config->register_settings_message ) ); ?>" /></td>
 	  </tr>
 	  <tr>
-		<th scope="row"><?php esc_html_e( 'User Name', 'wp-similar-basic-auth' ); ?></th>
-		<td><input type="text" name="<?php echo $hax_wsba_config->register_settings_user_name; ?>"  value="<?php echo esc_attr( get_option( $hax_wsba_config->register_settings_user_name ) ); ?>" /> <?php esc_html_e( "(alphanumerics, numbers, symbols(except < > & \" '). Can't be blank)", 'wp-similar-basic-auth' ); ?></td>
+  		<th scope="row"><?php esc_html_e( 'User Name', 'wp-similar-basic-auth' ); ?></th>
+  		<td><input type="text" name="<?php echo $hax_wsba_config->register_settings_user_name; ?>"  value="<?php echo esc_attr( get_option( $hax_wsba_config->register_settings_user_name ) ); ?>" /> <?php esc_html_e( "(alphanumerics, numbers, symbols(except < > & \" '). Can't be blank)", 'wp-similar-basic-auth' ); ?></td>
 	  </tr>
 	  <tr>
-		<th scope="row"><?php esc_html_e( 'Password', 'wp-similar-basic-auth' ); ?></th>
-		<!-- Do not use type="password" if disable autocomplete for Chrome. -->
-		<td>
-		  <p>
-			<input type="radio" id="current-password" name="select_password_action" value="checked-current-password" checked="checked">
-			  <label for="current-password"><?php esc_html_e( 'No Change (Use current password)', 'wp-similar-basic-auth' ); ?></label>
-		  </p>
-			<input type="radio" id="new-password" name="select_password_action" value="checked-new-password">
-			  <label for="new-password"><?php esc_html_e( 'New Password', 'wp-similar-basic-auth' ); ?>: </label>
-			<input type="text" autocomplete="off" id="new-password-text" name="<?php echo $hax_wsba_config->register_settings_password_text; ?>" oninput="autoRadioButtonChecked(this.value ,'current-password', 'new-password');">
-				<?php esc_html_e( "(alphanumerics, numbers, symbols(except < > & \" '). Can't be blank)", 'wp-similar-basic-auth' ); ?>
-		</td>
+  		<th scope="row"><?php esc_html_e( 'Password', 'wp-similar-basic-auth' ); ?></th>
+  		<!-- Do not use type="password" if disable autocomplete for Chrome. -->
+  		<td>
+  		  <p>
+  			<input type="radio" id="current-password" name="select_password_action" value="checked-current-password" checked="checked">
+  			  <label for="current-password"><?php esc_html_e( 'No Change (Use current password)', 'wp-similar-basic-auth' ); ?></label>
+  		  </p>
+  			<input type="radio" id="new-password" name="select_password_action" value="checked-new-password">
+  			  <label for="new-password"><?php esc_html_e( 'New Password', 'wp-similar-basic-auth' ); ?>: </label>
+  			<input type="text" autocomplete="off" id="new-password-text" name="<?php echo $hax_wsba_config->register_settings_password_text; ?>" oninput="autoRadioButtonChecked(this.value ,'current-password', 'new-password');">
+  				<?php esc_html_e( "(alphanumerics, numbers, symbols(except < > & \" '). Can't be blank)", 'wp-similar-basic-auth' ); ?>
+  		</td>
 	  </tr>
 	</table>
 
